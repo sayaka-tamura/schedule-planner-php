b05f676a3f7e22c07bbc385966fed5673e732828
sayaka-tamura <arccycle@gmail.com>
2021-08-22 13:06:58 +0900

    created a spaces for the last week

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
       // 月初日（1日）の曜日を取得
       $wd1 = date("w", mktime(0, 0, 0, $m, 1, $y));
-      var_dump($wd1);
+      // var_dump($wd1);
 
       // その数だけ空白を表示
       for($i = 1; $i <= $wd1; $i++){
@@ -22,7 +22,7 @@
       }
       
       // 今日が土曜日の場合は
-      if(date("w", mktime(0, 0, 0, $m, $d, $y))==6){
+      if(date("w", mktime(0, 0, 0, $m, $d, $y)) == 6){
         // 週を終了
         echo "</tr>";
 
@@ -30,6 +30,12 @@
         if(checkdate($m, $d+1, $y)){
           echo "<tr>";
         }
+      }
+
+      // 最後の週の土曜日まで移動
+      $wdx = date("w", mktime(0, 0, 0, $m+1, 0, $y));
+      for($i = 1; $i = 7 - $wdx; $i++){
+        echo "<td>  </td>";
       }
     ?>
   </tr>
