af9fe71edea5d75f11025d58c81efc87546955eb
sayaka-tamura <arccycle@gmail.com>
2021-08-22 12:52:11 +0900

    separated the calendar line for each week

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
         echo "<td>$d</td>";
         $d++;
       }
+      
+      // 今日が土曜日の場合は
+      if(date("w", mktime(0, 0, 0, $m, $d, $y))==6){
+        // 週を終了
+        echo "</tr>";
+
+        // さらに次の週がある場合は新たな行を準備
+        if(checkdate($m, $d+1, $y)){
+          echo "<tr>";
+        }
+      }
     ?>
   </tr>
 </table>
