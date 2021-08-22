bed000a3dbdaadaba0ccc63e3e8bd8d1e67c8c0f
sayaka-tamura <arccycle@gmail.com>
2021-08-22 12:41:55 +0900

    created a calender for Feb, 2021

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-  echo 'Hello World! I\'m here!';
-  echo "Problem: does not connect Git Deploy and Heroku";
-  echo "Was it solved?"
-?>
+<table border="1">
+  <tr>
+    <?php
+      $y = 2021;
+      $m = 2;
+      $d = 1;
+
+      while(checkdate($m, $d, $y)){
+        echo "<td>$d</td>";
+        d++;
+      }
+    ?>
+  </tr>
+</table>
